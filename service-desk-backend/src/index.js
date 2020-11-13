fb13980d4dc992eb20f2fcae22352202ba0e26de
Gabriel Gurris <ggurris@ptecnologico.local>
2020-11-13 12:07:47 -0300

    Criando rotas

--- service-desk-backend/src/index.js
+++ service-desk-backend/src/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+
+const authController = require('./controllers/auth');
+
+app.use('/auth', authController);
+/*  criando o acesso de authController atraves de 
+    /auth/sign-in
+    /auth/sign-up
+ */
+
 app.get('/', (req, res) => {
     return res.json('Api running....');
 })
 
-db.sequelize.sync().then(() => {
-    app.listen(3001, () => {
-        console.log('Listening on port 3000');
-    });
-})     
+app.listen(3001, () => {
+    console.log('Listening on port 3001');
+});
