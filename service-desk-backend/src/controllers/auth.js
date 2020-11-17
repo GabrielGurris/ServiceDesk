57eece678f1657f6ef7df311e21b060b885c66ad
GabrielGurris <gabriel_gurris@outlook.com>
2020-11-17 16:50:24 -0300

    Instalando Hapi

--- service-desk-backend/src/controllers/auth.js
+++ service-desk-backend/src/controllers/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const { Account } = require('../models');
+const { accountSignUp } = require('../validator/account');
 
 const router = express.Router();
 
@@ -10,7 +11,7 @@ router.get('/sign-in', (req, res) => {
     return res.json('Sign in.');
 });
 
-router.get('/sign-up', async (req, res) => {
+router.get('/sign-up', accountSignUp /*um middleware apenas para essa rota*/, async (req, res) => {
     const { name, email, address, naddress, neighborhood, cep, phone1, phone2, password } = req.body;
 
     const account = await Account.findOne({ where: { email } });
